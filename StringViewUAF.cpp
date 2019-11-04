77631ed6021efac4381878456fe018c92d793e01
Károly Fejes <karoly.fejes@gmail.com>
2019-11-04 11:22:22 +0100

    update

--- StringViewUAF.cpp
+++ StringViewUAF.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include <string>
-#include <string_view>
+#include <experimental/string_view>
+
+using namespace std;
+using namespace experimental;
 
 int main() {
-  std::string s = "Hellooooooooooooooo ";
-  std::string_view sv = s + "World\n";
-  std::cout << sv;
+  string s = "Hellooooooooooooooo ";
+  string_view sv = s + "World\n";
+  cout << sv;
 }
