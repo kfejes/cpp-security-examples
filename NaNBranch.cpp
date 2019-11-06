609ac7e7c7f0e44e277a362f091d7647e4c635ee
Károly Fejes <kfejes@inf.u-szeged.hu>
2019-11-06 12:32:39 +0100

    nanbranch is added

---
+++ NaNBranch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace  std;
+
+auto IsGreaterThan(float value) {
+
+    if (value >= 10.0) {
+        cout << "Branch True" << endl;
+        return true;        
+    } else if (value < 10.0) {
+        cout << "Branch False" << endl;
+        return false;        
+    } else {
+        cout << "What Else?" << endl;
+    } 
+}
+
+int main() {
+
+    float value = NAN;
+  
+    auto retVal = IsGreaterThan(4.2);
+    auto retVal1 = IsGreaterThan(14.2);
+    auto retValNan = IsGreaterThan(value);
+
+    cout << retValNan << endl;
+
+    return 0;
+}
